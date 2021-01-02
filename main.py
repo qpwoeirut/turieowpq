e3d12e5a91f5500f4308f93f96c316b16e31441c
qpwoeirut <stanleyzhong8@gmail.com>
2021-01-02 15:41:25 -0800

    more stuff

--- main.py
+++ main.py
@@ -25,16 +25,14 @@ async def on_ready():
 
         guild_count += 1
 
-    print(f"HelloWorldBot is in {guild_count} guilds.")
-
 
 def is_hm(text):
     return set(text) <= set("hm") and len(text) >= 2
 
 
 WORDS = ["scary", "lel", "lol", "aiya", "bruh", "breh", "oop", "<:notlikeduck:764579516755738664>",
-         "<:tourist_mad:764579517515300914>"]
-EMOTES = [":notlikeduck:", ":tourist_mad:"]
+         "<:tourist_mad:764579517515300914>", "<:confus:764579516755738664>", "<:foncus:766364807732527114>"]
+EMOTES = [":notlikeduck:", ":tourist_mad:", ":confus:", ":foncus:"]
 WORD_CHANCE = int(len(WORDS) * 1.25)
 
 
@@ -44,14 +42,15 @@ async def on_message(message):
         return
     #with open("log.txt", 'a') as f:
     #print(f"{message.author} said: {message.content}", file=f)
-    if any([emote in message.content for emote in itertools.chain(EMOTES, WORDS)]):
+    msg = message.content.lower();
+    if any([emote in msg for emote in itertools.chain(EMOTES, WORDS)]):
         #print("word matched", file=f)
         idx = random.randint(0, WORD_CHANCE - 1)
         if idx < len(WORDS):
             reply = WORDS[idx]
         else:
             reply = message.content
-    elif is_hm(message.content):
+    elif is_hm(msg):
         #print("hm matched", file=f)
         if random.randint(1, 10) == 1:
             reply = "yumm"
@@ -62,17 +61,24 @@ async def on_message(message):
                 reply += 'm' * random.randint(1, reply_size // 2)
             reply += 'h'
             reply += 'm' * random.randint(1, reply_size)
-    elif "sir" in message.content.lower():
+    elif "sir" in msg:
         #print("sir matched", file=f)
         reply = f"hello {message.author.mention} sir"
-    elif 'q' in message.content.lower() and random.randint(1, 3) == 1:
+    elif "sad" in msg:
+        #print("sad matched, file=f)
+        reply = f"{random.choice(['so', 'very'])} sad!"
+    elif 'q' in msg and random.randint(1, 3) == 1:
         #print("q matched", file=f)
         reply = "q is the best letter!"
     elif random.randint(1, 5) == 1:
         #print("random word", file=f)
         reply = random.choice(WORDS)
-    elif ' ' not in message.content and random.randint(1, 5) == 1:
+    elif ' ' not in message.content and ':' not in message.content and random.randint(1, 8) == 1:
         reply = message.content[::-1]
+    elif ' ' in message.content and random.randint(1, 10) == 1:
+        words = message.content.split()
+        words.reverse()
+        reply = ' '.join(words)
     else:
         return
     #print(f"sent {reply}", file=f)
