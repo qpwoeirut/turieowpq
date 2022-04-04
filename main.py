76f494ca5a9a94cb82f93affeb7168014a6d4b0a
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-04 12:13:57 -0700

    Add music example from the internet

--- main.py
+++ main.py
@@ -4,25 +4,34 @@
 # SOLVE aiohttp.client_exceptions.ClientConnectorCertificateError
 # https://stackoverflow.com/questions/59411362/ssl-certificate-verify-failed-certificate-verify-failed-unable-to-get-local-i
 
-import discord
+from discord.ext import commands
 
 from get_token import DISCORD_TOKEN
+from music import Music
 from spammer import spam
 
-client = discord.Client()
+client = commands.Bot(command_prefix=commands.when_mentioned_or("turi "),
+                      description='turieowpq')
 
 
 @client.event
 async def on_ready():
+    print(f"Logged in as {client.user} with id {client.user.id}")
     for guild in client.guilds:
         print(f"- {guild.id} (name: {guild.name})")
 
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
+    if message.author.bot:  # don't reply to bots
         return
+
     await spam(message)
 
+    await client.process_commands(message)
+    # https://stackoverflow.com/questions/49331096/why-does-on-message-stop-commands-from-working
 
-client.run(DISCORD_TOKEN)
+
+if __name__ == '__main__':
+    client.add_cog(Music(client))
+    client.run(DISCORD_TOKEN)
