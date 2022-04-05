d99616f143d8903310c57d8e327d5bacb3213596
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-04 20:26:04 -0700

    Don't delete responses

--- YTDLSource.py
+++ YTDLSource.py
@@ -58,7 +58,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
             # take first item from a playlist
             data = data['entries'][0]
 
-        await ctx.send(f'```ini\n[Added {data["title"]} to the Queue.]\n```', delete_after=15)
+        await ctx.send(f'```ini\n[Added {data["title"]} to the Queue.]\n```')
 
         if download:
             source = ytdl.prepare_filename(data)
