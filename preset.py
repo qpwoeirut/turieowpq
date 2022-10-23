461d6974babba326f84423293faeda6d634c60b1
qpwoeirut <stanleyzhong8@gmail.com>
2022-10-23 13:20:45 -0700

    Update preset list

--- preset.py
+++ preset.py
@@ -1,17 +1,20 @@
 TAYLOR_SWIFT = {
     "taylor swift": "https://www.youtube.com/playlist?list=OLAK5uy_l320Kcg2IKwpIRR07SD-ZejNX0cxRF32c",
-    "red": "https://www.youtube.com/playlist?list=OLAK5uy_kvYl6SDSIWUZ7loEGQlwzF7j04xzWvoZQ",
     "fearless": "https://www.youtube.com/playlist?list=OLAK5uy_lUwH9j_s3ZEeayUSm5o93gtQVz0If_kd8",
-    "evermore": "https://www.youtube.com/playlist?list=OLAK5uy_m-vSVOiVeY_z2lPgThmS6Nn0TJExXZtOg",
-    "folklore": "https://www.youtube.com/playlist?list=OLAK5uy_nWgO-2lNMsx90439Yx0xTWCGIktUc74e8",
-    "lover": "https://www.youtube.com/playlist?list=OLAK5uy_nHHWc9S0Nw7oLyLYBptkJ4DpkQeoL1Igw",
-    "reputation": "https://www.youtube.com/playlist?list=OLAK5uy_kyYsExXByLh2281MMfi0QvZJF5epEUxbk",
-    "1989": "https://www.youtube.com/playlist?list=PLqyng5B_ZuT_3fRojFoMweV4WXa-QQ1kq",
-    "red original": "https://www.youtube.com/playlist?list=OLAK5uy_mwwCV3ci_DoOhgq27DRqnrVG3QOR_S2hQ",
+    "red": "https://www.youtube.com/playlist?list=OLAK5uy_kvYl6SDSIWUZ7loEGQlwzF7j04xzWvoZQ",
     "speak now": "https://www.youtube.com/playlist?list=OLAK5uy_k_sq8Sp6KDtHZIxW6ovITiJhl6SIC-5gw",
+    "1989": "https://www.youtube.com/playlist?list=PLqyng5B_ZuT_3fRojFoMweV4WXa-QQ1kq",
+    "reputation": "https://www.youtube.com/playlist?list=OLAK5uy_kyYsExXByLh2281MMfi0QvZJF5epEUxbk",
+    "lover": "https://www.youtube.com/playlist?list=OLAK5uy_nHHWc9S0Nw7oLyLYBptkJ4DpkQeoL1Igw",
+    "folklore": "https://www.youtube.com/playlist?list=OLAK5uy_nWgO-2lNMsx90439Yx0xTWCGIktUc74e8",
+    "evermore": "https://www.youtube.com/playlist?list=OLAK5uy_m-vSVOiVeY_z2lPgThmS6Nn0TJExXZtOg",
+    "midnights": "https://www.youtube.com/playlist?list=PLINj2JJM1jxP5aYiX47uBCRu9g8JItDWp",
+    "red original": "https://www.youtube.com/playlist?list=OLAK5uy_mwwCV3ci_DoOhgq27DRqnrVG3QOR_S2hQ",
     "fearless original": "https://www.youtube.com/playlist?list=OLAK5uy_kymlVnEd_mmMQfc4GJJPTNOW-ipnOhsrY",
     "red piano": "https://www.youtube.com/watch?v=FSbpyATwAJs",
-    "fearless piano": "https://www.youtube.com/watch?v=YBrTNiKBdog"
+    "fearless piano": "https://www.youtube.com/watch?v=YBrTNiKBdog",
+    "1989 piano": "https://www.youtube.com/watch?v=uHFawHdyWhY",
+    "folklore evermore piano": "https://www.youtube.com/playlist?list=PLwerpwcWAr1pU1tDypyH-CSG8PcaY4h5O"
 }
 
 THE_SCORE = {
